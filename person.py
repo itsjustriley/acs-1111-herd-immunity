d49f33fdc175783e4a35f56d29fb15d32bf44f17
itsjustriley <142259664+itsjustriley@users.noreply.github.com>
2023-11-29 18:31:33 -0400

    finished person.py less stretch challenges

--- person.py
+++ person.py
@@ -7,20 +7,20 @@ class Person(object):
     # Define a person. 
     def __init__(self, _id, is_vaccinated, infection = None):
         # A person has an id, is_vaccinated and possibly an infection
-        self._id = _id  # int
-        # TODO Define the other attributes of a person here
-        pass
+        self._id = _id  
+        self.is_vaccinated = is_vaccinated
+        self.infection = infection
+        self.is_alive = True
 
     def did_survive_infection(self):
-        # This method checks if a person survived an infection. 
-        # TODO Only called if infection attribute is not None.
-        # Check generate a random number between 0.0 - 1.0
-        # If the number is less than the mortality rate of the 
-        # person's infection they have passed away. 
-        # Otherwise they have survived infection and they are now vaccinated. 
-        # Set their properties to show this
-        # TODO: The method Should return a Boolean showing if they survived.
-        pass
+        if self.infection is not None:
+            if random.random() < self.infection.mortality_rate:
+                self.is_alive = False
+                return False
+            else:
+                self.is_vaccinated = True
+                self.infection = None
+                return True
 
 if __name__ == "__main__":
     # This section is incomplete finish it and use it to test your Person class
@@ -34,6 +34,10 @@ if __name__ == "__main__":
     # Create an unvaccinated person and test their attributes
     unvaccinated_person = Person(2, False)
     # TODO Test unvaccinated_person's attributes here...
+    assert unvaccinated_person._id == 2
+    assert unvaccinated_person.is_alive is True
+    assert unvaccinated_person.is_vaccinated is False
+    assert unvaccinated_person.infection is None
 
     # Test an infected person. An infected person has an infection/virus
     # Create a Virus object to give a Person object an infection
@@ -43,31 +47,35 @@ if __name__ == "__main__":
     # TODO: complete your own assert statements that test
     # the values of each attribute
     # assert ...
+    assert infected_person._id == 3
+    assert infected_person.is_alive is True
+    assert infected_person.is_vaccinated is False
+    assert infected_person.infection is virus
+
 
     # You need to check the survival of an infected person. Since the chance
     # of survival is random you need to check a group of people. 
     # Create a list to hold 100 people. Use the loop below to make 100 people
     people = []
-    for i in range(1, 100):
-        # TODO Make a person with an infection
-        # TODO Append the person to the people list
-        pass
+    for i in range(0, 100):
+        people.append(Person(i, False, virus))
 
     # Now that you have a list of 100 people. Resolve whether the Person 
     # survives the infection or not by looping over the people list. 
 
-    # for person in people:
-    #     # For each person call that person's did_survive_infection method
-    #     survived = person.did_survive_infection()
+    did_survive = 0
+    did_not_survive = 0
+    for person in people:
+        # For each person call that person's did_survive_infection method
+        survived = person.did_survive_infection()
+        if survived:
+            did_survive += 1
+        else:
+            did_not_survive += 1
 
-    # Count the people that survived and did not survive: 
-   
-    # did_survived = 0
-    # did_not_survive = 0
+    print(f"Survived: {did_survive}")
+    print(f"Did not survive: {did_not_survive}")
 
-    # TODO Loop over all of the people 
-    # TODO If a person is_alive True add one to did_survive
-    # TODO If a person is_alive False add one to did_not_survive
 
     # TODO When the loop is complete print your results.
     # The results should roughly match the mortality rate of the virus
